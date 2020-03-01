1de5f8617f08ee87f736284369c096ef7e0d8b51
Araya_001 <s5730213001@phuket.psu.ac.th>
2020-03-01 15:37:47 +0700

    Add files via upload

---
+++ function-arguments.js
@@ -0,0 +1,6 @@
+function math (firstArg, secondArg,thirdArg) {  
+    firstArg += secondArg*thirdArg;
+    return firstArg;
+    //console.log(firstArg, secondArg)  
+  }
+  console.log(math(53,61,67));
